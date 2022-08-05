024e1a7492f758daee9616e9c0ca38c2b0bbb179
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2022-08-05 18:06:08 +0300

    Initial commit. Create simple app structure. Implement components and styling.

---
+++ Applications/RNCourse/App.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    width: "80%",
+    padding: 8,
+    marginRight: 8,
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: "#cccccc",
+  },
+});
