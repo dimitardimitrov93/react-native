50a4d3d085580f70a0c7104ad71d33d6dbc5d5f4
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2022-09-30 16:41:36 +0300

    Structure and style components.

--- Applications/RNCourse/GuessTheNumberApp/screens/StartGameScreen.js
+++ Applications/RNCourse/GuessTheNumberApp/screens/StartGameScreen.js
@@ -1,29 +1,57 @@
+import { useState } from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 import { PrimaryButton } from "./components/PrimaryButton";
 
 export const StartGameScreen = () => {
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <View style={styles.buttonsContainer}>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
-    // flexDirection: "row",
-    // justifyContent: "space-between",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 32,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    fontSize: 33,
+    fontWeight: "bold",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 24,
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
   },
 });
--- Applications/RNCourse/GuessTheNumberApp/screens/components/PrimaryButton.js
+++ Applications/RNCourse/GuessTheNumberApp/screens/components/PrimaryButton.js
@@ -1,9 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 export const PrimaryButton = ({ children }) => {
+  const pressHandler = () => {
+    console.log("Pressed!");
+  };
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.outerContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.innerContainer, styles.pressed]
+            : styles.innerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    marginVertical: 4,
+    marginHorizontal: 8,
+    borderRadius: 28,
+    overflow: "hidden",
+    elevation: 2,
+  },
+  innerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
